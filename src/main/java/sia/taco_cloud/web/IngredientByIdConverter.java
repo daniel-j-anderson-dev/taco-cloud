9952bc87d64bb4b47fbe7eb124f0fa985cf8db44
Daniel Anderson <122058981+daniel-j-anderson-dev@users.noreply.github.com>
2026-04-07 08:00:39 -0600

    Add IngredientByIdConverter to get Ingredient instances from the design form submition

---
+++ src/main/java/sia/taco_cloud/web/IngredientByIdConverter.java
@@ -0,0 +1,23 @@
+package sia.taco_cloud.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import tacos.Ingredient;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+    private Map<String, Ingredient> ingredientMap = new HashMap<>();
+
+    public IngredientByIdConverter() {
+        this.ingredientMap = MockIngredientDataBase.INGREDIENT_BY_ID;
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        return this.ingredientMap.get(id);
+    }
+}
