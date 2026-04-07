048cf3ddf42ee06a5a15ecb0861ade227d6e327f
Daniel Anderson <122058981+daniel-j-anderson-dev@users.noreply.github.com>
2026-04-07 07:59:14 -0600

    Refactor DesignTacoController to use MockIngredientDataBase

--- src/main/java/sia/taco_cloud/web/DesignTacoController.java
+++ src/main/java/sia/taco_cloud/web/DesignTacoController.java
@@ -1,54 +1,26 @@
 package sia.taco_cloud.web;
 
-import sia.taco_cloud.Ingredient;
-import sia.taco_cloud.Taco;
-import sia.taco_cloud.TacoOrder;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
+import tacos.Taco;
+import tacos.TacoOrder;
+
 @Slf4j
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
-    private static final List<Ingredient> INGREDIENTS = List.of(
-            new Ingredient("FLTO", "Flour Tortilla", Ingredient.Kind.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Ingredient.Kind.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Ingredient.Kind.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Ingredient.Kind.PROTEIN),
-            new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Kind.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Ingredient.Kind.VEGGIES),
-            new Ingredient("CHED", "Cheddar", Ingredient.Kind.CHEESE),
-            new Ingredient("JACK", "Monterrey Jack", Ingredient.Kind.CHEESE),
-            new Ingredient("SLSA", "Salsa", Ingredient.Kind.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Ingredient.Kind.SAUCE));
-
-    private static final Map<String, List<Ingredient>> INGREDIENTS_BY_KIND = Arrays
-            .stream(Ingredient.Kind.values())
-            .collect(Collectors.toMap(
-                    // key producer
-                    kind -> kind.toString().toLowerCase(),
-                    // value producer
-                    kind -> INGREDIENTS
-                            .stream()
-                            .filter(ingredient -> ingredient.getKind().equals(kind))
-                            .collect(Collectors.toList())));
-
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        model.addAllAttributes(INGREDIENTS_BY_KIND);
+        model.addAllAttributes(MockIngredientDataBase.INGREDIENTS_BY_KIND);
     }
 
     @ModelAttribute(name = "tacoOrder")
@@ -64,5 +36,12 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
     }
 }
