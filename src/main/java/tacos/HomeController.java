fd48814cac5441a51828bf46ceb23a43e25125d8
Daniel Anderson <122058981+daniel-j-anderson-dev@users.noreply.github.com>
2026-04-08 06:47:53 -0600

    Add POST mapping to HomeController

--- src/main/java/tacos/HomeController.java
+++ src/main/java/tacos/HomeController.java
@@ -2,12 +2,20 @@ package tacos;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping
     public String home() {
         return "home";
     }
     
+    @PostMapping
+    public String sendToDesign() {
+        return "redirect:design";
+    }
 }
