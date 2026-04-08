2e93a8041b93c406ab58d24056a07f1e8819eedc
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-04-08 12:16:06 -0600

    added ingredient repo impl

---
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -0,0 +1,37 @@
+package tacos.data;
+
+import java.util.Optional;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import tacos.Ingredient;
+
+@Repository
+public class JdbcIngredientRepository implements IngredientRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Iterable<Ingredient> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Optional<Ingredient> findById(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public Ingredient save(Ingredient ingredient) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+}
