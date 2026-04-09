fa7e9668b85e466c548be5b3965953671057a411
Daniel Anderson <122058981+daniel-j-anderson-dev@users.noreply.github.com>
2026-04-09 07:12:47 -0600

    Rename tables and columns in schema.sql to match default naming scheme of spring data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,37 +1,38 @@
-create table if not exists TacoOrder (
-    id identity primary key,
-    deliveryName varchar(50) not null,
-    deliveryStreet varchar(50) not null,
-    deliveryCity varchar(50) not null,
-    deliveryState varchar(2) not null,
-    deliveryZip varchar(10) not null,
-    creditCardNumber varchar(16) not null,
-    creditCardExpiration varchar(5) not null,
-    creditCardValidationValue varchar(3) not null,
-    placedAt timestamp not null
+create table if not exists Taco_Order (
+    id                           identity,
+    delivery_Name                varchar(50) not null,
+    delivery_Street              varchar(50) not null,
+    delivery_City                varchar(50) not null,
+    delivery_State               varchar(2)  not null,
+    delivery_Zip                 varchar(10) not null,
+    credit_card_number           varchar(16) not null,
+    credit_card_expiration       varchar(5)  not null,
+    credit_card_validation_value varchar(3)  not null,
+    placed_at                    timestamp   not null
 );
 
 create table if not exists Taco (
-    id identity primary key,
-    name varchar(50) not null,
-    tacoOrder bigint not null,
-    tacoOrderKey bigint not null,
-    createdAt timestamp not null
+    id             identity,
+    name           varchar(50) not null,
+    taco_order     bigint      not null,
+    taco_order_key bigint      not null,
+    created_at     timestamp   not null
 );
 
-create table if not exists IngredientRef (
+create table if not exists Ingredient_Ref (
     ingredient varchar(4) not null,
-    taco bigint not null,
-    tacoKey bigint not null
+    taco       bigint     not null,
+    taco_key   bigint     not null
 );
 
 create table if not exists Ingredient (
-    id varchar(4) not null primary key,
+    id   varchar(4)  not null,
     name varchar(25) not null,
     kind varchar(10) not null
 );
 
 alter table Taco
-    add foreign key (tacoOrder) references TacoOrder(id);
-alter table IngredientRef
+    add foreign key (taco_order) references Taco_Order(id);
+alter table Ingredient_Ref
     add foreign key (ingredient) references Ingredient(id);
+
