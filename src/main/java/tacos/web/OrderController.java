06a5bade873ba49c0d6b0767fb208f2b54d48824
Daniel Anderson <122058981+daniel-j-anderson-dev@users.noreply.github.com>
2026-04-09 08:31:19 -0600

    Set order placed date when submitting order

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,5 +1,7 @@
 package tacos.web;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +41,7 @@ public class OrderController {
         if (validationErrors.hasErrors()) {
             return "orderForm";
         }
+        order.setPlacedAt(new Date());
         var _altered_order = this.orderRepository.save(order);
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
